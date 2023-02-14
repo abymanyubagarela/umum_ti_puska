2e9823fcb5c94f37bacf5ef272643d8ce73ca931
Abimanyu Bagarela Anjaya Putra <abimanyu.putra@bpk.go.id>
2023-02-14 13:28:51 +0800

    login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -8,25 +9,50 @@ class LoginController extends Controller
 {
     public function index()
     {
-        if(Auth::check()){
+        if (Auth::check()) {
             return redirect('backend/dashboard');
         };
         return view('backend/login');
-
-
     }
 
     public function authenticate(Request $request)
     {
-        
+
+
         $credentials = $request->validate(['email' => 'required|email', 'password' => 'required']);
 
-        if (Auth::attempt(['account_email' => $credentials['email'], 'password' => $credentials['password']]))
-        {
-            $request->session()->regenerate();
+        define('LDAP_HOST', 'ldap://dcrodc.bpk.go.id:389');
+        $username =  $credentials['email'];
+        $password =  $credentials['password'];
+        $dc = ldap_connect(LDAP_HOST);
+        ldap_set_option($dc, LDAP_OPT_REFERRALS, 0);
+        ldap_set_option($dc, LDAP_OPT_PROTOCOL_VERSION, 3);
+        $bind = @ldap_bind($dc, $username, $password);
 
-            return redirect()->intended('backend/dashboard/puska');
+        if ($bind) {
+            $cn = substr($username, 0, strpos($username, '@'));
+            $res = ldap_search($dc, 'dc=bpk,dc=go,dc=id', "(samaccountname={$cn})", ['description']);
+            $data = ldap_get_entries($dc, $res);
+
+            dd($data);
+            if ($data['count'] > 0) {
+                $nip_pendek = trim($data[0]['description'][0]);
+                // TODO auth ok
+                // dd($nip_pendek);
+                $user = Accounts::where('account_email', '=', $username)->first();
+
+                if (Auth::loginUsingId($user->id)) {
+
+                    $request->session()->regenerate();
+
+                    return redirect()->intended('backend/dashboard');
+                }
+            }
         }
+
+        @ldap_close($dc);
+
+
 
         return redirect()->back()->withInput()->with('loginError', 'Login failed!');
     }
@@ -37,7 +63,6 @@ class LoginController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('/login');
-
     }
 
     public function userIndex()
@@ -49,16 +74,12 @@ class LoginController extends Controller
     {
         $credentials = $request->validate(['email' => 'required|email', 'password' => 'required']);
 
-        if (Auth::attempt(['account_email' => $credentials['email'], 'password' => $credentials['password']]))
-        {
+        if (Auth::attempt(['account_email' => $credentials['email'], 'password' => $credentials['password']])) {
             $request->session()->regenerate();
 
             return redirect()->intended('/dashboard');
         }
 
         return redirect()->back()->withInput($request->all)->with('loginError', 'Login failed!');
-
     }
-
 }
-
