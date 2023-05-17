be9ec34709f16795c8d726440edd62a18d8dd059
Abimanyu Bagarela Anjaya Putra <abimanyu.putra@bpk.go.id>
2023-05-17 10:43:43 +0800

    bg

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -28,7 +28,7 @@
 			<div class="container">
 				<div class="loginbox">
 					<div class="login-left">
-						<img class="img-fluid" src="/assets/img/login.png" alt="Logo">
+						<img class="img-fluid" src="/assets/img/logo-bpk.png" alt="Logo">
 					</div>
 					<div class="login-right">
 						<div class="login-right-wrap">
